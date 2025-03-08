60c1a97fbddddd87d65d6942b6baa562b8479a53
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-08 15:21:52 -0300

    add Numbers.java and Strings.java

---
+++ Strings.java
@@ -0,0 +1,20 @@
+public class Strings {
+	public static void main(String[] args) {
+
+		String a = "Hello";
+		String b = "Bye";
+
+		// Use .equals to compare, not ==
+		// Reason: String is an object, not a primitive type
+		System.out.println( a.equals(b) );
+
+		// Concatenation
+		System.out.println(a + " " + b);
+
+		// Concatenation with an integer. Can mix string and int directly
+		int num = 21;
+		System.out.println(a + " " + num);
+
+
+	}
+}
