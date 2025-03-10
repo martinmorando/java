2a1f43ff918f28dd7b1c16c7296f046db8134af3
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-10 16:27:39 -0300

    add string representation and basic methods

--- Spaceship.java
+++ Spaceship.java
@@ -17,12 +17,33 @@ public class Spaceship {
 		name = spaceshipName;
 	}
 
-	public static void main(String[] args) {		
+	// String representation of the object
+	// A special method that prints custom message instead of object name and position in memory
+	public String toString() {
+		return "I'm " + name;
+	}
+
+	// Method to change spaceship name
+	public void changeName(String newName) {
+		name = newName;
+	}
+
+	// Return current countdown
+	public int currentCountdown() {
+		return countdown;
+	}
+
+	public static void main(String[] args) {
+
 		Spaceship curiosity = new Spaceship("Curiosity & Beyond", 5, true); // Uses first constructor
 		System.out.println(curiosity.name);
+		curiosity.changeName("Curiosity & Beyond Inc.");
+		System.out.println(curiosity); // Prints new name
 		
 		Spaceship conquer = new Spaceship("Conquer Enterprises"); // Uses second constructor
 		System.out.println(conquer.name);
+		System.out.println(curiosity.currentCountdown() + " days to liftoff.");
+
 	}
 
 }
