4852dbb1b232911ec95e3b3f900ceb26bce31dc7
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-14 23:52:33 -0300

    add loops

---
+++ 8-Loops/Plane.java
@@ -0,0 +1,27 @@
+/*
+    Loops
+*/
+public class Plane {
+    
+    public static void main(String[] args) {
+
+        
+        int n = 10;
+
+        // For loop is identical to PHPs
+        for (int i=0; i<n; i++) {
+            System.out.println( "For loop: " + i );
+        }
+
+
+        // While loop is identical to PHPs
+        int i = 0;
+        while (i < n) {
+            System.out.println( "While loop: " + i );
+            i++;
+        }
+        
+
+    }
+
+}
