6ad8c6ef9a1dbe59be893550f373405f985ae712
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-16 19:41:44 -0300

    add examples of how to iterate over an array and an array list

--- 8-Loops/Plane.java
+++ 8-Loops/Plane.java
@@ -2,27 +2,42 @@
     Loops
 */
 
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class Plane {
     
     public static void main(String[] args) {
 
-        
         int n = 10;
 
-        // For loop is identical to PHPs
+        // For loop is identical to PHP's
         for (int i=0; i<n; i++) {
             System.out.println( "For loop: " + i );
         }
 
 
-        // While loop is identical to PHPs
+        // While loop is identical to PHP's
         int i = 0;
         while (i < n) {
             System.out.println( "While loop: " + i );
             i++;
         }
-        
 
+
+        // Iterate over an array
+        int[] numbers = {21, 22, 23, 24};
+        for (int c=0; c<numbers.length; c++) {
+            System.out.println( "Array: " + numbers[c] );
+        }
+
+
+        // Iterate over an ArrayList 
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(4,5,6,7,8));
+        for (int d=0; d<list.size(); d++) {
+            System.out.println( "ArrayList: " + list.get(d) );
+        }
+        
     }
 
 }
