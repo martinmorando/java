ee5416820803dfdca246c0ef6ad1336d4cdb24bf
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-18 19:24:51 -0300

    add equalsIgnoreCase(), concat(), indexOf(), charAt(), and substring() to 2-Strings/Strings.java

--- 2-Strings/Strings.java
+++ 2-Strings/Strings.java
@@ -25,7 +25,26 @@ public class Strings {
         System.out.println( a.toUpperCase() );  // "HELLO"
 
         // IMPORTANT: As Strings is an object, use .equals to compare, not ==
-        System.out.println( a.equals(b) );      // false
+        System.out.println( a.equals(b) );                  // false
+        System.out.println( a.equalsIgnoreCase("hello") );  // true
+
+        // IMPORTANT: concat() does not changes the object
+        // Remember: strings are immutable objects 
+        System.out.println( a.concat(b) );      // "HelloBye"
+
+        // Returns index of first match
+        System.out.println( a.indexOf("e") );   // 1
+        System.out.println( a.indexOf("llo") ); // 2
+
+        // Returns character from specified index
+        System.out.println( a.charAt(1) );      // "e"
+
+        // Extract a substring from a string
+        // - From specified index to the end 
+        System.out.println( a.substring(1) );   // "ello"
+
+        // - From specified index to another specified index
+        System.out.println( a.substring(1,3) ); // "el"
 
     }
 
