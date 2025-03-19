ad9250d84196c364bbd2c9cefdbbf14771a639cf
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-19 20:58:18 -0300

    add 9-Access

---
+++ 9-Access/Helicopter.java
@@ -0,0 +1,10 @@
+/*
+    Access/Helicopter
+*/
+
+public class Helicopter {
+
+    public int a = 1;   // Public variable
+    private int b = 2;  // Private variable
+
+}
---
+++ 9-Access/Plane.java
@@ -0,0 +1,15 @@
+/*
+    Access/Plane
+*/
+
+public class Plane {
+    
+    public static void main(String[] args) {
+
+        Helicopter h = new Helicopter();
+        System.out.println(h.a);   // 1
+        // System.out.println(h.b);   // Won't compile
+        
+    }
+
+}
